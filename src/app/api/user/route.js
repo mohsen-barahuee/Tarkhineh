97c4c329f927ba5e6f0e7f1b0c20376a206af834
Mohsen barahuee <nakel3920@gmail.com>
2025-01-31 20:16:17 +0330

    new files

---
+++ src/app/api/user/route.js
@@ -0,0 +1,25 @@
+import connectToDB from "../../../../utils/db"
+import UserModel from "../../../../models/user"
+import { NextResponse } from "next/server"
+import { cookies } from "next/headers"
+import { verifyToken } from "../../../../utils/auth"
+
+
+export async function GET(request) {
+    connectToDB()
+    const cookie = cookies();
+    const getToken = await cookie.catch()
+    if (!getToken._parsed.get('token').value) {
+        return NextResponse.json({ messsage: "Token Is Empty" })
+    }
+    else {
+        const tokenPayload = verifyToken(getToken._parsed.get('token').value)
+        const userAccount = await UserModel.findOne({ phoneNumber: tokenPayload.phoneNumber })
+
+        return Response.json({ userAccount });
+    }
+
+
+    return Response.json({ message: "USER ROUTE" })
+
+}
