1efe0b5fb288941f988de6e3fd499ae9480a39da
Mohsen barahuee <nakel3920@gmail.com>
2025-02-21 21:38:14 +0330

    api for update user

---
+++ src/app/api/[id]/route.js
@@ -0,0 +1,17 @@
+import connectToDB from "../../../../utils/db"
+import UserModel from "../../../../models/user"
+
+export async function PUT(request, { params }) {
+
+    connectToDB()
+    const { id } = await params
+    const body = await request.json()
+
+    const { username, phoneNumber, firstName, lastName, email, birthDay, } = body
+
+    const updateUser = await UserModel.findOneAndUpdate({ _id: id }, { username, phoneNumber, firstName, lastName, email, birthDay, })
+
+
+    return Response.json({ updateUser })
+
+}
